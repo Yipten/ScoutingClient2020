c03230b9d0476b23bce5a4993d3a2daf84087494
Andrew S <yipten@outlook.com>
2020-01-27 11:40:28 -0500

    get rid of update command (no need for button)

--- ScoutingClient2020/ViewModels/TeamStatsViewModel.cs
+++ ScoutingClient2020/ViewModels/TeamStatsViewModel.cs
@@ -1,16 +1,11 @@
-﻿using ScoutingClient2020.Commands;
-using ScoutingClient2020.Static;
+﻿using ScoutingClient2020.Static;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows.Input;
 
 namespace ScoutingClient2020.ViewModels {
-	class TeamStatsViewModel : INotifyPropertyChanged {
+	class TeamStatsViewModel {
 		public List<int> Teams { get; set; }
 		public int SelectedTeam { get => _selectedTeam; set { _selectedTeam = value; Update(); } }
 		public Stat TestStat { get; set; }
-
-		public ICommand UpdateCommand { get; set; }
 
 		private readonly Stat[] _stats;
 		private int _selectedTeam;
@@ -19,8 +14,6 @@ namespace ScoutingClient2020.ViewModels {
 			Teams = new List<int>();
 			TestStat = new Stat("SELECT 100.0 * SUM(CrossHabLine) / COUNT() FROM RawData WHERE TeamNumber = {0} AND StartPosition BETWEEN 3 AND 5;", "Test Value", "units");
 			_stats = new Stat[] { TestStat };
-
-			UpdateCommand = new UpdateTeamStatsCommand(this);
 
 			UpdateTeamsList();
 		}
@@ -46,15 +39,5 @@ namespace ScoutingClient2020.ViewModels {
 			foreach (Stat stat in _stats)
 				stat.Update(_selectedTeam);
 		}
-
-		#region INotifyPropertyChanged Members
-
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		private void OnPropertyChanged(string propertyName) {
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-
-		#endregion
 	}
 }
