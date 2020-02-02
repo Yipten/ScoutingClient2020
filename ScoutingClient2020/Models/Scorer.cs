3e8719f49d62490150adb0126036bd86ba91bc53
Andrew S <yipten@outlook.com>
2020-02-02 12:59:06 -0500

    score now updates whenever anything is changed

--- ScoutingClient2020/Models/Scorer.cs
+++ ScoutingClient2020/Models/Scorer.cs
@@ -2,20 +2,32 @@
 
 namespace ScoutingClient2020.Models {
 	class Scorer : INotifyPropertyChanged {
-		public bool InitLine { get; set; }
-		public int AutoBottom { get; set; }
-		public int AutoOuter { get; set; }
-		public int AutoInner { get; set; }
-		public int TeleBottom { get; set; }
-		public int TeleOuter { get; set; }
-		public int TeleInner { get; set; }
-		public bool RotationControl { get; set; }
-		public bool PositionControl { get; set; }
-		public bool Park { get; set; }
-		public bool Climb { get; set; }
-		public bool Balanced { get; set; }
+		public bool InitLine { get => _initLine; set { _initLine = value; UpdateScore(); } }
+		public int AutoBottom { get => _autoBottom; set { _autoBottom = value; UpdateScore(); } }
+		public int AutoOuter { get => _autoOuter; set { _autoOuter = value; UpdateScore(); } }
+		public int AutoInner { get => _autoInner; set { _autoInner = value; UpdateScore(); } }
+		public int TeleBottom { get => _teleBottom; set { _teleBottom = value; UpdateScore(); } }
+		public int TeleOuter { get => _teleOuter; set { _teleOuter = value; UpdateScore(); } }
+		public int TeleInner { get => _teleInner; set { _teleInner = value; UpdateScore(); } }
+		public bool RotationControl { get => _rotationControl; set { _rotationControl = value; UpdateScore(); } }
+		public bool PositionControl { get => _positionControl; set { _positionControl = value; UpdateScore(); } }
+		public bool Parked { get => _parked; set { _parked = value; UpdateScore(); } }
+		public bool Climbed { get => _climbed; set { _climbed = value; UpdateScore(); } }
+		public bool Balanced { get => _balanced; set { _balanced = value; UpdateScore(); } }
 		public string ScoreStr { get => _scoreStr; set { _scoreStr = value; OnPropertyChanged(nameof(ScoreStr)); } }
 
+		private bool _initLine;
+		private int _autoBottom;
+		private int _autoOuter;
+		private int _autoInner;
+		private int _teleBottom;
+		private int _teleOuter;
+		private int _teleInner;
+		private bool _rotationControl;
+		private bool _positionControl;
+		private bool _parked;
+		private bool _climbed;
+		private bool _balanced;
 		private int _score;
 		private string _scoreStr;
 
@@ -29,8 +41,8 @@ namespace ScoutingClient2020.Models {
 			TeleInner = 0;
 			RotationControl = false;
 			PositionControl = false;
-			Park = false;
-			Climb = false;
+			Parked = false;
+			Climbed = false;
 			Balanced = false;
 		}
 
@@ -45,8 +57,8 @@ namespace ScoutingClient2020.Models {
 				TeleInner * 3 +
 				(RotationControl ? 10 : 0) +
 				(PositionControl ? 20 : 0) +
-				(Park ? 5 : 0) +
-				(Climb ? 25 : 0) +
+				(Parked ? 5 : 0) +
+				(Climbed ? 25 : 0) +
 				(Balanced ? 15 : 0);
 			ScoreStr = "Score: " + _score.ToString();
 		}
--- ScoutingClient2020/ViewModels/ScoringViewModel.cs
+++ ScoutingClient2020/ViewModels/ScoringViewModel.cs
@@ -1,19 +1,11 @@
-﻿using ScoutingClient2020.Commands;
-using ScoutingClient2020.Models;
-using System.Windows.Input;
+﻿using ScoutingClient2020.Models;
 
 namespace ScoutingClient2020.ViewModels {
 	class ScoringViewModel {
 		public Scorer Scorer { get; set; }
 
-		public ICommand UpdateScoreCommand { get; private set; }
-
 		public ScoringViewModel() {
 			Scorer = new Scorer();
-
-			UpdateScoreCommand = new UpdateScoreCommand(this);
-
-			UpdateScore();
 		}
 
 		public void UpdateScore() {
