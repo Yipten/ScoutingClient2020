444bff99eaa5922a7c704a75e8cd74be6056f487
Andrew S <yipten@outlook.com>
2020-02-18 19:12:14 -0500

    set up match history page

--- ScoutingClient2020/ViewModels/MatchHistoryViewModel.cs
+++ ScoutingClient2020/ViewModels/MatchHistoryViewModel.cs
@@ -1,5 +1,21 @@
 ﻿namespace ScoutingClient2020.ViewModels {
 	class MatchHistoryViewModel {
+		public int SelectedMatch { get => _selectedTeam; set { _selectedTeam = value; Update(); } }
 
+		private int _selectedTeam;
+
+		/// <summary>
+		/// Initializes a new instance of the MatchHistoryViewModel class.
+		/// </summary>
+		public MatchHistoryViewModel() {
+
+		}
+
+		/// <summary>
+		/// Updates all data fields on the page.
+		/// </summary>
+		public void Update() {
+
+		}
 	}
 }
